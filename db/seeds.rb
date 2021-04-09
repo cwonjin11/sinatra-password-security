047a8d33616f6b0dc795224b754994735fb37ac9
Wonjin Cho <cwonjin11@gmail.com>
2021-04-09 10:36:34 -0400

    commit tree 7b71a7c21ae21d54a9ed43a4ffa13eb04f83b1c4

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+user1 = User.create(:username => "skittles123", :password => "password")
+
+user2 = User.create(:username => "flatiron4lyfe", :password => "password")
+
+user3 = User.create(:username => "kittens1265", :password => "password")
+
+puts "good!"
